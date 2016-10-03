ffb87aaa1edb23f186e93e31e171c422cc382d78
Andyll96 <andysl_1996@hotmail.com>
2016-10-03 18:16:28 -0400

    Update
    Everything works except for the hashing, and the termination of a record
    if the wrong password has been inputed for 5 attempts, also if you get
    it wrong once but get it right any of the other times it erases the
    whole table

--- Lab_1_v3/main.c
+++ Lab_1_v3/main.c
@@ -5,7 +5,7 @@
 #define MAX_INPUT 40
 
 void writeFile();
-void inputPass();
+int inputPass(int i, int newpasswrd);
 void inputID();
 void userInput();
 void printDB();
@@ -29,7 +29,7 @@ int main()
 
     printf("\n\n\tWelcome to CPS_633, Lab 1\t\n\n");
 
-    userInput();
+    inputID();
     writeFile();
     printDB();
 
@@ -58,7 +58,6 @@ void printDB()
 {
     printf("\nUsername\tPassword\n");
     int i;
-    int databaseLength = 0;
 
     for (i = 0; i <= MAX_RECORDS; i++)
     {
@@ -73,9 +72,11 @@ void printDB()
     }
 }
 
-void inputPass()
+int inputPass(int i, int newpasswrd)
 {
     int correct = 1, strLength;
+    int record = i;
+    int newpass = newpasswrd;
 
     printf("Please Enter Your Password (no special characters): ");
     fgets(passInput, MAX_INPUT, stdin);
@@ -88,45 +89,212 @@ void inputPass()
 
     while (correct)
     {
-        if (strLength > 12) //Password longer than 12 characters
+        int k;
+        int specialCase = 1;
+        for (k = 0; k <= strLength; k++) //Searches for special characters
         {
-            printf("This password is too long, the characters after the 12th has been cut off\n");
-            int i;
-            for (i = 0; i < MAX_RECORDS; i++)
+            switch (passInput[k])
             {
-                if (passwordArray[i][0] == '\0')
-                {
-                    strncpy(passwordArray[i], passInput, 12);
-                    break;
-                }
-                else
-                {
-                    continue;
-                }
-            }
-            correct = 0;
-        }
-        else //Password shorter than 12 characters
-        {
-            int i, j;
-            for (j = strLength; j <= 12; j++)
-            {
-                passInput[j] = '-';
-            }
+            case '~':
+                printf("Special Character(s) Found\n");
+                specialCase = 0;
+                break;
+            case '!':
+                printf("Special Character(s) Found\n");
+                specialCase = 0;
+                break;
+            case '`':
+                printf("Special Character(s) Found\n");
+                specialCase = 0;
+                break;
+            case '@':
+                printf("Special Character(s) Found\n");
+                specialCase = 0;
+                break;
+            case '#':
+                printf("Special Character(s) Found\n");
+                specialCase = 0;
+                break;
+            case '$':
+                printf("Special Character(s) Found\n");
+                specialCase = 0;
+                break;
+            case '%':
+                printf("Special Character(s) Found\n");
+                specialCase = 0;
+                break;
+            case '^':
+                printf("Special Character(s) Found\n");
+                specialCase = 0;
+                break;
+            case '&':
+                printf("Special Character(s) Found\n");
+                specialCase = 0;
+                break;
+            case '*':
+                printf("Special Character(s) Found\n");
+                specialCase = 0;
+                break;
+            case '(':
+                printf("Special Character(s) Found\n");
+                specialCase = 0;
+                break;
+            case ')':
+                printf("Special Character(s) Found\n");
+                specialCase = 0;
+                break;
+            case '-':
+                printf("Special Character(s) Found\n");
+                specialCase = 0;
+                break;
+            case '_':
+                printf("Special Character(s) Found\n");
+                specialCase = 0;
+                break;
+            case '=':
+                printf("Special Character(s) Found\n");
+                specialCase = 0;
+                break;
+            case '+':
+                printf("Special Character(s) Found\n");
+                specialCase = 0;
+                break;
+            case '[':
+                printf("Special Character(s) Found\n");
+                specialCase = 0;
+                break;
+            case ']':
+                printf("Special Character(s) Found\n");
+                specialCase = 0;
+                break;
+            case '{':
+                printf("Special Character(s) Found\n");
+                specialCase = 0;
+                break;
+            case '}':
+                printf("Special Character(s) Found\n");
+                specialCase = 0;
+                break;
+            case '|':
+                printf("Special Character(s) Found\n");
+                specialCase = 0;
+                break;
+            case '\\':
+                printf("Special Character(s) Found\n");
+                specialCase = 0;
+                break;
+            case ':':
+                printf("Special Character(s) Found\n");
+                specialCase = 0;
+                break;
+            case ';':
+                printf("Special Character(s) Found\n");
+                specialCase = 0;
+                break;
 
-            for (i = 0; i < MAX_RECORDS; i++)
-            {
-                if (passwordArray[i][0] == '\0')
-                {
-                    strncpy(passwordArray[i], passInput, 12);
-                    break;
-                }
-                else
-                {
-                    continue;
-                }
+            case '"':
+                printf("Special Character(s) Found\n");
+                specialCase = 0;
+                break;
+            case ',':
+                printf("Special Character(s) Found\n");
+                specialCase = 0;
+                break;
+            case '<':
+                printf("Special Character(s) Found\n");
+                specialCase = 0;
+                break;
+            case '.':
+                printf("Special Character(s) Found\n");
+                specialCase = 0;
+                break;
+            case '>':
+                printf("Special Character(s) Found\n");
+                specialCase = 0;
+                break;
+            case '?':
+                printf("Special Character(s) Found\n");
+                specialCase = 0;
+                break;
+            case '/':
+                printf("Special Character(s) Found\n");
+                specialCase = 0;
+                break;
             }
-            correct = 0;
+            if (specialCase == 0)
+                break;
+        }
+        if (specialCase != 0) //If there are no special characters
+        {
+            if (strLength > 12) //Password longer than 12 characters
+            {
+                printf("This password is too long, the characters after the 12th has been cut off\n");
+                int i;
+                for (i = 0; i < MAX_RECORDS; i++)
+                {
+                    if (passwordArray[i][0] == '\0')
+                    {
+                        strncpy(passwordArray[i], passInput, 12);
+                        break;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                correct = 0;
+            }
+            else //Password acceptably shorter than 12 characters
+            {
+                if (passwordArray[record][0] == '\0') //If record is empty
+                {
+                    strncpy(passwordArray[record], passInput, 12);
+                    return 0;
+                }
+                else //If record not empty, compare
+                {
+                    int r;
+                    for (r = 0; r < 12; r++) //unpads for compairison
+                    {
+                        if (passwordArray[record][r] == '-')
+                            passwordArray[record][r] = '\0';
+                    }
+
+                    if (strcmp(passInput, passwordArray[record]) == 0) //If match
+                    {
+                        printf("correct password\n");
+                        int j;
+                        for (j = strLength; j < 12; j++) //Pads password with "-"
+                        {
+                            passInput[j] = '-';
+                        }
+                        strncpy(passwordArray[record], passInput, 12);
+                        return 0;
+                    }
+                    else //If not match
+                    {
+                        if (newpass == 0)
+                        {
+                            int j;
+                            for (j = strLength; j < 12; j++) //Pads password with "-"
+                            {
+                                passInput[j] = '-';
+                            }
+                            strncpy(passwordArray[record], passInput, 12);
+                            return 0;
+                        }
+                        else if (newpass == 1)
+                        {
+                            return 1;
+                        }
+                    }
+                }
+                correct = 0;
+            }
+        }
+        else //If there are special characters
+        {
+            return 1;
         }
     }
 }
@@ -144,7 +312,7 @@ void inputID()
 
     while (correct)
     {
-        if (strLength > 32)
+        if (strLength > 32) //If longer than 32 characters
         {
             printf("This Username ID is longer than 32 characters\n");
             printf("Please Enter Your Username ID: ");
@@ -155,7 +323,7 @@ void inputID()
             if (strLength > 0 && IDInput[strLength] == '\n')
                 IDInput[strLength] = '\0';
         }
-        else if (strLength < 4)
+        else if (strLength < 4) //If shorter than 4 characters
         {
             printf("This Username ID is shorter than 4 characters\n");
             printf("Please Enter Your Username ID: ");
@@ -166,30 +334,64 @@ void inputID()
             if (strLength > 0 && IDInput[strLength] == '\n')
                 IDInput[strLength] = '\0';
         }
-        else
+        else //If acceptable length
         {
             int i;
             for (i = 0; i < MAX_RECORDS; i++)
             {
-                if (IDArray[i][0] == '\0')
+                if (IDArray[i][0] != '\0') //If element occupied, compare
+                {
+                    if (strcmp(IDArray[i], IDInput) == 0) // If the same
+                    {
+                        printf("Match Found\n");
+                        int attempts = 4, cmpr;
+                        while (attempts > -1)
+                        {
+                            printf("You have %d attempts \n", attempts + 1);
+                            cmpr = inputPass(i, 1);
+                            if (cmpr == 1 && attempts > 0) //wrong attempt, still has attempts
+                            {
+                                attempts--;
+                                continue;
+                            }
+                            else if (cmpr == 1 && attempts == 0) //failed 5 attempts
+                            {
+                                printf("failure, account will be erased");
+                                break;
+                            }
+                            else if (cmpr == 0 && attempts > 0)
+                            {
+                                printf("success!!\nTime for a new Password\n");
+                                int test = 1;
+                                while (test == 1)
+                                {
+                                    test = inputPass(i, 0);
+                                }
+                                break;
+                            }
+                        }
+                        correct = 0;
+                        break;
+                    }
+                    else //If not the same
+                    {
+                        continue;
+                    }
+                }
+                else //If element empty, insert
                 {
                     strcpy(IDArray[i], IDInput);
+                    int test = 1;
+                    while (test == 1)
+                    {
+                        test = inputPass(i, 0);
+                    }
                     break;
-                }
-                else
-                {
-                    continue;
                 }
             }
             correct = 0;
         }
     }
-}
-
-void userInput()
-{
-    inputID();
-    inputPass();
 }
 
 void readFile()
